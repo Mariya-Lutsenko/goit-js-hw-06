da9a1dda458b68688b2d9eef15c9c5c996a9d8ec
Mariya-Lutsenko <mashkakorchagina@gmail.com>
2022-11-04 20:46:08 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,19 +1,16 @@
 const textInput = document.querySelector('#validation-input');
-const verifyInput = textInput.getAttribute('data-length');
+const verifyInput = Number(textInput.getAttribute('data-length'));
 console.log(verifyInput);
 
 textInput.addEventListener('blur', onInputBlur)
 
 function onInputBlur (event) {
-    if (event.currentTarget.value.length == verifyInput) {
+	if (event.currentTarget.value.length === verifyInput) {
+		textInput.classList.remove('invalid')
 		textInput.classList.add('valid')
-		if (textInput.classList.contains('invalid')) {
-			textInput.classList.remove('invalid')
-		}
-	} else {
-		if (textInput.classList.contains('valid')) {
-			textInput.classList.remove('valid')
-		}
-		textInput.classList.add('invalid')
 	}
+	else {textInput.classList.remove('valid')
+	    textInput.classList.add('invalid')}
 }
+
+console.log();
